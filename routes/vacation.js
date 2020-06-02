7756641778d0a63326089beb4a38fe724c10d897
Julia Kadilnikova <aracabana@gmail.com>
2020-06-02 20:04:03 +0300

    create home page, create-vacation page

--- routes/vacation.js
+++ routes/vacation.js
@@ -1,21 +1,32 @@
-const express                = require('express');
-const router                 = express.Router();
-const access                 = require('../routes/access');
-const { vacationController } = require('../controllers');
+const express                       = require('express');
+const router                        = express.Router();
+const access                        = require('../routes/access');
+const { vacationController }        = require('../controllers');
+const { geoNames, countries } = require('../helpers/geonames');
 
-router.get('/', access, function(request, response) {
-    response.render('create-vacation', {
-        pageTitle: 'Создать отпуск',
-        title: 'Создать отпуск',
-        login: request.session.login,
-        btn: {
-            link: '/',
-            title: 'Вернуться к списку отпусков',
-            icon: 'fa-angle-left',
-            class: 'btn-light'
-        },
-        script: 'create-vacation'
-    });
+router.get('/', access, async function(request, response) {
+    try {
+        const countryNames = await countries.getAll();
+        console.log(countryNames);
+        response.render('create-vacation', {
+            pageTitle: 'Создать отпуск',
+            title: 'Создать отпуск',
+            login: request.session.login,
+            btn: {
+                link: '/',
+                title: 'Вернуться к списку отпусков',
+                icon: 'fa-angle-left',
+                class: 'btn-light'
+            },
+            countryNames,
+            script: 'create-vacation'
+        });
+    } catch (err) {
+        response.json({
+            ok: false,
+            caption: err.message
+        })
+    }
 });
 router.get('/all', access, vacationController.getAllByUserId);
 
